4e9abc9564d7182fb389dafa1098e97054d373c3
Stuart Campbell <stuart@harto.org>
2013-06-06 23:40:40 +1000

    Move requirements into setup.py

--- setup.py
+++ setup.py
@@ -12,17 +12,27 @@ setup(
     author='99designs',
     author_email='stuart.campbell@99designs.com',
     description='A retro-style bug leaderboard',
+    long_description=slurp('README.md'),
     license='MIT',
     keywords='bug dashboard leaderboard github jira',
     url='https://github.com/99designs/githubsurvivor',
+    classifiers=('Development Status :: 4 - Beta',),
+
     packages=('survivor',
               'survivor.backends',
               'survivor.models',
               'survivor.tasks',
               'survivor.web'),
+
     package_dir={'': 'src'},
-    long_description=slurp('README.md'),
-    classifiers=('Development Status :: 4 - Beta',),
+
+    install_requires=('Flask==0.9',
+                      'mongoengine==0.8.1',
+                      'python-dateutil==2.1',
+                      'PyGithub==1.16.0',
+                      'iso8601==0.1.4',
+                      'jira-python==0.13'),
+
     entry_points={
         'console_scripts': [
             'githubsurvivor=survivor.web:main',
